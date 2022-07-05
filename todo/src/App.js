b42e8ffddb9eca4efc5b60221601641a3a76307a
HamHax <hamo.haxverdyan2000@gmail.com>
2022-07-05 13:47:18 +0400

    adding new user logic to our user list

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import Button from "./components/Button/Button";
 import Inlinetext from "./components/Inlinetext/Inlinetext";
 import Input from "./components/Input/Input";
@@ -16,9 +16,11 @@ class App extends Component {
         {name: 'Ash', surname: 'Saghatelyan', age: 23, id: 3},
       ],
       nextUserId: 4,
-      searchName: ''
-
+      searchName: '',      
     }
+    this.nextUserName = React.createRef('');
+    this.nextUserSurname = React.createRef('');
+    this.nextUserAge = React.createRef('');
   }
 
   deleteHandler = (id) => {
@@ -38,12 +40,39 @@ class App extends Component {
       return   item.name.toLocaleLowerCase().includes(this.state.searchName.toLocaleLowerCase()) 
   }
 
+  addUserHandler = () => {
+    const newUser = {
+      name: this.nextUserName.current.value,
+      surname: this.nextUserSurname.current.value,
+      age: this.nextUserAge.current.value,
+      id: this.state.nextUserId
+    }
+    this.setState({
+      nextUserId: this.state.nextUserId + 1,
+      userlist: this.state.userlist.concat(newUser)
+    })
+    
+
+  }
+
 
 
   render() {
     return (
        <Wrapper>
+        <Inlinetext>search user by name</Inlinetext>
         <Input onChange={this.changeName}  type='text'/>
+        <br />
+        <Inlinetext> new user name </Inlinetext>
+        <Input propsRef={this.nextUserName} type='text' />
+        <br />
+        <Inlinetext> new user surname </Inlinetext>
+        <Input propsRef={this.nextUserSurname} type='text' />
+        <br />
+        <Inlinetext> new user age </Inlinetext>
+        <Input propsRef={this.nextUserAge} type='number' />
+        <br />
+        <Button onClick={this.addUserHandler}> add new user </Button>
          <List>
           {
             this.state.userlist.filter(this.filterUser).map(user => {
