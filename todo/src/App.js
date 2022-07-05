fe44eff86f63ef211507fb58155c4377415ad60c
HamHax <hamo.haxverdyan2000@gmail.com>
2022-07-05 13:05:58 +0400

    add users rendering on screen method

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,15 +1,40 @@
 import { Component } from "react";
+import Inlinetext from "./components/Inlinetext/Inlinetext";
+import List from "./components/List/List";
+import Listitem from "./components/Listitem/Listitem";
+import Wrapper from "./ui/Wrapper/Wrapper";
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      userlist: [
+        {name: 'Ham', surname: 'Haxverdyan', age: 22, id: 1},
+        {name: 'Narek', surname: 'Ghazaryan', age: 32, id: 2},
+        {name: 'Ash', surname: 'Saghatelyan', age: 23, id: 3},
+      ],
+      nextUserId: 4,
+
+    }
   }
 
   render() {
     return (
-      <section>
-
-      </section>
+       <Wrapper>
+         <List>
+          {
+            this.state.userlist.map(user => {
+              return (
+                <Listitem key={user.id}>
+                   <Inlinetext> {user.name} </Inlinetext>
+                   <Inlinetext> {user.surname} </Inlinetext>
+                   <Inlinetext> {user.age} </Inlinetext>
+                </Listitem>
+              )
+            })
+          }
+         </List>
+       </Wrapper>
     )
   }
 }
--- todo/src/ui/Wrapper/Wrapper.js
+++ todo/src/ui/Wrapper/Wrapper.js
@@ -3,7 +3,7 @@ import { Component } from "react";
 class Wrapper extends Component {
     render() {
         return (
-            this.props.childfren
+            this.props.children
         )
     }
 }
